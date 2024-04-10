c7657a62dbf0a86667e1cdad69ea0c3c8a15834a
GarryVeles <aligar39@gmail.com>
2024-04-10 13:22:08 +0200

    Update keymap.c KC_B

--- totem/keymaps/garry/keymap.c
+++ totem/keymaps/garry/keymap.c
@@ -159,7 +159,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
               TD(Q_ESC), KC_W,    KC_E,     KC_R,     KC_T,         KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  
               GUI_A,    ALT_S,    CTL_D,    SHT_F,    KC_G,         KC_H,     SHT_J,    CTL_K,    ALT_L,    GUI_S,    
-    RAISE2,   TD(Z_BSPC), CTL_X,  SHT_C,    KC_V,     TD(B_CTL),    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  LOWER2,
+    RAISE2,   TD(Z_BSPC), CTL_X,  SHT_C,    KC_V,     KC_B,    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  LOWER2,
                                   KC_DEL,   LOWER,    KC_SPC,       KC_BSPC,  RAISE,    KC_ENT
  ),
 
